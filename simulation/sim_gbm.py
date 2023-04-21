c8a2d2ca7ba65087908c647556277c44f918d1d4
Kenno <kenno.nicklas@gmail.com>
2023-04-21 12:33:03 +0200

    Removed redundant parameter from the function 'sim_gbm'.

--- simulation/sim_gbm.py
+++ simulation/sim_gbm.py
@@ -1,27 +1,24 @@
 import numpy as np
 
 
-def sim_gbm(t, x0, M, N, mu, sigma, seed=None):
+def sim_gbm(t, x0, N, mu, sigma, seed=None):
     """
     Function for simulating Geometric Brownian Motion over specified time steps and initial values of the processes.
     The simulation uses the discretized analytical solution,
         X(t+1) = X(t) * exp{(mu-0.5*sigma^2)*dt + sigma * W(t)}
-
     :param t:       time steps (including start and endpoints)
     :param x0:      Initial values of the processes
-    :param M:       Number of discretizations (time steps)
     :param N:       Number of simulations
     :param mu:      Drift coefficient
     :param sigma:   Diffusion coefficient (volatility)
     :param seed:    Seed used for replication of results
-
     :return:        numpy.array with dim (M+1, N).
                     Rows are time steps (including the start and end-point)
                     Columns are the number of simulations
     """
 
-    if not len(t) == M+1:
-        raise ValueError('Length of time steps must include both start and endpoint, that is have length of M+1.')
+    assert np.ndim(t) == 1, 'Time steps must be a 1-dimensional array.'
+    M = len(t) - 1
 
     rng = np.random.default_rng(seed=seed)
     Z = rng.standard_normal(size=(M, N))
@@ -51,5 +48,5 @@ if __name__ == '__main__':
     t = np.linspace(start=t0, stop=T, num=M+1, endpoint=True)
 
     # Perform simulation
-    x = sim_gbm(t=t, x0=x0, M=M, N=N, mu=mu, sigma=sigma, seed=seed)
+    x = sim_gbm(t=t, x0=x0, N=N, mu=mu, sigma=sigma, seed=seed)
     print(x)
