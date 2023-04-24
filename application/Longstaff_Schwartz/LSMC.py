4204b441c1b7204aab9e1f2e2240d6c49e9b1151
Andreas <andreas.axel@live.dk>
2023-04-24 15:34:20 +0200

    Change of index in order to handle exercise at time-0 issue

--- application/Longstaff_Schwartz/LSMC.py
+++ application/Longstaff_Schwartz/LSMC.py
@@ -1,5 +1,6 @@
 import numpy as np
-from application.options.payoff import european_payoff
+from options.payoff import european_payoff
+from simulation.sim_gbm import sim_gbm
 
 
 def lsmc(t, X, k, r, payoff_func, type, deg):
@@ -28,33 +29,31 @@ def lsmc(t, X, k, r, payoff_func, type, deg):
     # Initiate
     discount_factor = np.exp(-r * dt)
     payoff = payoff_func(X, k, type)
-    stopping_rule = np.full((M+1, N), False)
-    cashflow = np.full((M+1, N), np.nan)
+    stopping_rule = np.full((M, N), False)
+    cashflow = np.full((M, N), np.nan)
 
-    stopping_rule[M: ] = payoff[M, :] > 0
-    cashflow[M, :] = payoff[M, :]
+    stopping_rule[M-1: ] = payoff[M, :] > 0
+    cashflow[M-1, :] = payoff[M, :]
 
-    for j in range(M-1, -1, -1):
+    for j in range(M-1, 0, -1):
         itm = payoff[j, :] > 0
 
         # Perform regression
-        coeff = np.polyfit(x=X[j, itm], y=cashflow[j+1, itm] * discount_factor[-j], deg=deg)
+        coeff = np.polyfit(x=X[j, itm], y=cashflow[j, itm] * discount_factor[-j], deg=deg)
 
         # Determine stopping rule by
         # comparing the value of exercising with the expected value of continuation
         EV_cont = np.polyval(p=coeff, x=X[j, itm])
         exercise = payoff[j, itm]
 
-        stopping_rule[j, itm] = exercise > EV_cont
+        stopping_rule[j-1, itm] = exercise > EV_cont
 
         # Update cash-flow matrix
-        cashflow[j, :] = stopping_rule[j, :] * payoff[j, :]
+        cashflow[j-1, :] = stopping_rule[j-1, :] * payoff[j, :]
 
     # Format stopping rule and cashflow-matrix
     stopping_rule = np.cumsum(np.cumsum(stopping_rule, axis=0), axis=0) == 1
     cashflow = stopping_rule * cashflow
-
-    cashflow = cashflow[1:, :]
 
     # Calculate price
     price = np.mean(cashflow.T @ np.cumprod(discount_factor))
@@ -64,16 +63,33 @@ def lsmc(t, X, k, r, payoff_func, type, deg):
 
 if __name__ == '__main__':
     # Example from the Longstaff-Schwartz article
-    X = np.array((
-        [1.00, 1.00, 1.00, 1.00, 1.00, 1.00, 1.00, 1.00],
-        [1.09, 1.16, 1.22, 0.93, 1.11, 0.76, 0.92, 0.88],
-        [1.08, 1.26, 1.07, 0.97, 1.56, 0.77, 0.84, 1.22],
-        [1.34, 1.54, 1.03, 0.92, 1.52, 0.90, 1.01, 1.34]
-    ))
     k = 1.10
     r = 0.06
     t = np.linspace(start=0, stop=3, num=4)
     type = 'PUT'
     deg = 2
 
-    print(lsmc(t=t, X=X, k=k, r=r, payoff_func=european_payoff, type=type, deg=deg))
+
+    X = np.array((
+        [1.00, 1.00, 1.00, 1.00, 1.00, 1.00, 1.00, 1.00],
+        [1.09, 1.16, 1.22, 0.93, 1.11, 0.76, 0.92, 0.88],
+        [1.08, 1.26, 1.07, 0.97, 1.56, 0.77, 0.84, 1.22],
+        [1.34, 1.54, 1.03, 0.92, 1.52, 0.90, 1.01, 1.34]
+    ))
+    print("Price Longstaff-Schwarz: ", lsmc(t=t, X=X, k=k, r=r, payoff_func=european_payoff, type=type, deg=deg))
+
+
+    # simulating with GBM
+    x0 = 1
+    t0 = 0.0
+    T = 1.0
+    N = 10000
+    M = 252
+    sigma = 0.2
+    seed = 1234
+
+    t = np.linspace(start=t0, stop=T, num=M+1, endpoint=True)
+    X = sim_gbm(t=t, x0=1, N=N, mu=r, sigma=sigma, seed=seed)
+
+    print("Price with GBM simulation: ", lsmc(t=t, X=X, k=k, r=r, payoff_func=european_payoff, type=type, deg=deg))
+
