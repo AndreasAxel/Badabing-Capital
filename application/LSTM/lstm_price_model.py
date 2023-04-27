be42692f9f89c0d3cf6e83287651d34efc89570a
Andreas <andreas.axel@live.dk>
2023-04-27 17:23:00 +0200

    Add working LSTM pricer

--- application/LSTM/lstm_price_model.py
+++ application/LSTM/lstm_price_model.py
@@ -8,8 +8,8 @@ from pathlib import Path
 # Long-Short Term Memory network used for estimating deltas through back-propagation
 def LSTM_model(num_inputs, num_outputs, *args, **kwargs):
     model = tf.keras.Sequential([
-        layers.LSTM(units=32, input_shape=(1, num_inputs)), #LSTM layer
-        layers.Dense(units=num_outputs) # regular densely-connected NN layers
+        layers.LSTM(units=32, input_shape=(1, num_inputs)), # LSTM layer
+        layers.Dense(units=num_outputs) # Regular densely-connected NN layers
     ])
     return model
 
@@ -31,19 +31,11 @@ def LSTM_train_model(X_train, y_train, epochs = 1, batch_size = 100):
 # Test the LSTM model to price American options
 def LSTM_test_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
-    #y_pred = y_pred.reshape((len(y_pred), ))
-    print("y_pred shape: ", y_pred.shape)
-    print("y_test_shape:", y_test.shape)
-    #print("y_pred type: ", np.typename(y_pred))
-    print("y_pred: ", y_pred)
-    print("y_test:", y_test)
-    rmse = np.sqrt(np.mean(y_pred-y_test)**2)  # penalty function
+    rmse = np.sqrt(np.mean(y_pred-y_test)**2)  # Penalty function
     print(f"RMSE: {rmse:.4f}")
 
 
 def predict_price(model, input_data):
-    #input_data = np.array(input_data)
-    #input_data = np.reshape(input_data, (1, 1, input_data.shape[0]))
     price = model.predict(input_data)
     return price
 
@@ -56,7 +48,7 @@ if __name__ == "__main__":
     import_filename = Path(__file__).parent.parent.parent / "data/training_data_PUT.csv"
     data = np.genfromtxt(import_filename, delimiter = ",", skip_header = 0)
 
-    # Seperate option prices from input data
+    # Separate option prices from input data
     X = data[:, :-1]
     y = data[:, -1]
     y = y.reshape((len(y), 1)) # Reshape in order to match shape of predicted y's
