6e1fc37d0f4f3ad5a0684b2cbfe7412a79f44994
Kenno <kenno.nicklas@gmail.com>
2023-05-11 12:57:31 +0200

    Made LSMC into a class

--- application/simulation/sim_ISD.py
+++ application/simulation/sim_ISD.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.neighbors import KernelDensity
 
+
 def sim_ISD(N, x0:float, alpha, seed=None, *args, **kwargs):
     """
     Creates N initially state dispersed variables (ISD).
@@ -23,8 +24,49 @@ def sim_ISD(N, x0:float, alpha, seed=None, *args, **kwargs):
     X = float(x0) + alpha * kernel
     return X
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    plt.hist(sim_ISD(N=1000, x0=100.0, alpha=5), bins=50)
-    plt.show()
+    from application.simulation.sim_gbm import sim_gbm
+    from application.Longstaff_Schwartz.LSMC import LSMC
+    from application.Longstaff_Schwartz.utils.fit_predict import fit_poly, pred_poly
+    from application.options.payoff import european_payoff
+    t0 = 0.0
+    T = 1.0
+    x0 = 40.0
+    N = 100000
+    M = 50
+    r = 0.06
+    sigma = 0.2
+    K = 40.0
+    seed = None
+    deg_lsmc = 9
+    deg_stentoft = 9
+    option_type = 'PUT'
+
+    alpha = 5.0
+
+    t = np.linspace(start=t0, stop=T, num=M + 1, endpoint=True)
+
+    # Simulate paths (with Initial State Dispersion)
+    x_isd = sim_ISD(N=N, x0=x0, alpha=alpha)
+    X = sim_gbm(t=t, x0=x_isd, N=N, mu=r, sigma=sigma, seed=seed)
+
+    # LSMC
+    lsmc = LSMC(X=X, t=t, K=K, r=r, payoff_func=european_payoff, option_type=option_type)
+    lsmc.run_backwards(fit_func=fit_poly, pred_func=pred_poly, deg=deg_lsmc)
+
+    # Get cashflow at stopping-time
+    cf = lsmc.payoff[1:]
+    cf_tau = np.zeros(N)
+    cf_tau[~np.isnan(lsmc.pathwise_opt_stopping_idx)] = cf[lsmc.opt_stopping_rule]
+
+    # Calculate discount factor
+    df = [np.exp(-r*tau) if ~np.isnan(tau) else 0.0 for tau in lsmc.pathwise_opt_stopping_time]
+
+    # Calculate discounted cashflow (present value)
+    cf_pv = cf_tau * df
+
+    fit = fit_poly(x=x_isd - x0, y=cf_pv, deg=deg_stentoft)  # coefficients `b`
+    price = pred_poly(x=x0-x0, fit=fit)
+    print(price)
