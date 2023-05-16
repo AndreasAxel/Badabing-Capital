f7a740a931d6b3f8ccb3cbaf7f82d4c110e5a086
seba6029 <79045074+seba6029@users.noreply.github.com>
2023-05-16 16:49:24 +0200

    compatible stentoft

--- application/simulation/sim_ISD.py
+++ application/simulation/sim_ISD.py
@@ -1,8 +1,11 @@
 import numpy as np
-from sklearn.neighbors import KernelDensity
+from application.Longstaff_Schwartz.LSMC import LSMC
+from application.Longstaff_Schwartz.utils.fit_predict import fit_poly, pred_poly
+from application.simulation.sim_gbm import GBM
+from application.options.payoff import european_payoff
 
 
-def sim_ISD(N, x0:float, alpha, seed=None, *args, **kwargs):
+def ISD(N, x0, alpha, seed=None):
     """
     Creates N initially state dispersed variables (ISD).
     With reference to Latourneau & Stentoft (2023)
@@ -11,13 +14,12 @@ def sim_ISD(N, x0:float, alpha, seed=None, *args, **kwargs):
     :param x0:      init. value
     :param alpha:   band width dispersion
     :param seed:    seed
-    :param args:
-    :param kwargs:
+
     :return:        size N vector of state dispersed vars around x0
     """
-
-    # epanechnikov kernel
     vecUnif = np.random.default_rng(seed=seed).uniform(low=0, high=1, size=N)
+
+    # Epanechnikov kernel
     kernel = 2 * np.sin( np.arcsin(2 * vecUnif -1) / 3)
 
     # Initial state dispersion
@@ -26,11 +28,6 @@ def sim_ISD(N, x0:float, alpha, seed=None, *args, **kwargs):
 
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    from application.simulation.sim_gbm import sim_gbm
-    from application.Longstaff_Schwartz.LSMC import LSMC
-    from application.Longstaff_Schwartz.utils.fit_predict import fit_poly, pred_poly
-    from application.options.payoff import european_payoff
     t0 = 0.0
     T = 1.0
     x0 = 40.0
@@ -49,11 +46,12 @@ if __name__ == '__main__':
     t = np.linspace(start=t0, stop=T, num=M + 1, endpoint=True)
 
     # Simulate paths (with Initial State Dispersion)
-    x_isd = sim_ISD(N=N, x0=x0, alpha=alpha)
-    X = sim_gbm(t=t, x0=x_isd, N=N, mu=r, sigma=sigma, seed=seed)
+    x_isd = ISD(N=N, x0=x0, alpha=alpha)
+    X = GBM(t=t, x0=x_isd, N=N, mu=r, sigma=sigma, seed=seed, use_av=True)
+    X.sim_exact()
 
     # LSMC
-    lsmc = LSMC(X=X, t=t, K=K, r=r, payoff_func=european_payoff, option_type=option_type)
+    lsmc = LSMC(simulator=X, K=K, r=r, payoff_func=european_payoff, option_type=option_type)
     lsmc.run_backwards(fit_func=fit_poly, pred_func=pred_poly, deg=deg_lsmc)
 
     # Get cashflow at stopping-time
@@ -63,14 +61,22 @@ if __name__ == '__main__':
 
     # Calculate discount factor
     df = [np.exp(-r*tau) if ~np.isnan(tau) else 0.0 for tau in lsmc.pathwise_opt_stopping_time]
+    # or "total" discounting?
+    # df = np.exp(-r*T)
 
     # Calculate discounted cashflow (present value)
     cf_pv = cf_tau * df
 
+    # Compute price and greeks from estimates                           
+    s0 = x0 # Spot value
+
     coef_price = fit_poly(x=x_isd - x0, y=cf_pv, deg=deg_stentoft)  # coefficients `b`
-    price = pred_poly(x=x0 - x0, fit=coef_price)
+    price = pred_poly(x=s0 - x0, fit=coef_price)
 
     coef_delta = np.polyder(coef_price, 1)
-    delta = pred_poly(x=x0 - x0, fit=coef_delta)
+    delta = pred_poly(x=s0 - x0, fit=coef_delta)
 
+    coef_gamma = np.polyder(coef_price, 2)
+    gamma = pred_poly(x=s0 - x0, fit=coef_gamma)
 
+    print('price = {}, delta = {}, gamma = {}'.format(price, delta, gamma))
